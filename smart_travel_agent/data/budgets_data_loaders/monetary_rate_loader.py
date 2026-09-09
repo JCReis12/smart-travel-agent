cb68f8ece86efce78dbc84fad5220732bd9d70c0
João Carlos <joaocarlosreis2007@gmail.com>
2026-09-08 22:12:31 -0300

    feat: criação da função que carrega as taxas

---
+++ smart_travel_agent/data/budgets_data_loaders/monetary_rate_loader.py
@@ -0,0 +1,33 @@
+'''
+
+    TOOL responsável por converter moedas.
+    Recebe como parametro:
+        - currency_from: moeda de origem
+        - currency_to: moeda de destino
+        - amount: valor a ser convertido
+
+
+'''
+#https://www.exchangerate-api.com
+
+import requests
+
+def currency_conversion(currency_destination, currency_local, amount ) -> dict:
+
+    currency_url = f"https://v6.exchangerate-api.com/v6/609d5a8da24c1edae813d6a4/latest/{currency_local}"
+    
+    response = requests.get(currency_url)
+
+    if response.status_code != 200:
+        return{
+            "status" : "error",
+            "message" : "Erro ao buscar cotação da moeda"
+        }
+        
+    data = response.json()
+
+    if "conversion_rates" not in data:
+        return{
+            "status" : "error",
+            "message" : "Cotação não encontrada"
+        }
