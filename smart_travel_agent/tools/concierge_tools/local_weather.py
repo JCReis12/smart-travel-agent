acc61bff86a17763bc189bc58bb50cc5a276c0d9
João Carlos <joaocarlosreis2007@gmail.com>
2026-09-09 22:27:46 -0300

    fix: limpeza e retirada de dados e importações de dados diretos

--- smart_travel_agent/tools/concierge_tools/local_weather.py
+++ smart_travel_agent/tools/concierge_tools/local_weather.py
@@ -1,49 +1,7 @@
 import requests
 
-def get_coordinates(city: str) -> dict:
-
-    coord_url = (
-        f"https://geocoding-api.open-meteo.com/v1/search"
-        f"?name={city}"
-        f"&count=1"
-        f"&language=pt"
-        f"&format=json"
-        f"&utm_source=chatgpt.com"
-    )
-
-    coord_response = requests.get(coord_url)
-
-    if coord_response.status_code != 200:
-        return{
-            "status" : "error",
-            "message" : "Erro ao buscar coordenadas."
-        }
-
-    coord_data = coord_response.json()
-
-    if "results" not in coord_data:
-        return{
-            "status" : "error",
-            "message" : f"A cidade de {city} não foi encontrada"
-        }
-
-    coordinates = coord_data["results"][0]
-
-    return{
-        "status" : "success",
-        "latitude" : coordinates["latitude"],
-        "longitude" : coordinates["longitude"]
-    }
 
 def get_weather(latitude: float, longitude: float) -> dict:
-    '''
-    weather_url = (
-        f"https://api.open-meteo.com/v1/forecast"
-        f"?latitude={latitude}"
-        f"&longitude={longitude}"
-        f"&current_weather=true"
-    )
-    '''
 
     weather_url = (
         
