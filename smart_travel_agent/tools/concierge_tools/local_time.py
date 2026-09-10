e28af20b33496b2eed712d2983bd5fce502ec7b2
João Carlos <joaocarlosreis2007@gmail.com>
2026-09-09 22:25:07 -0300

    feat: adição de dados mockados padrão local para caso de erros e testes

--- smart_travel_agent/tools/concierge_tools/local_time.py
+++ smart_travel_agent/tools/concierge_tools/local_time.py
@@ -1,8 +1,73 @@
 from datetime import datetime
 import pytz
-from smart_travel_agent.data.concierge_data_loaders.timezones_loader import timezones
 
 def get_local_time(city: str) -> dict:
+
+    '''
+    timezones = {
+        "miami": "America/New_York",
+        "new york": "America/New_York",
+        "los angeles": "America/Los_Angeles",
+        "chicago": "America/Chicago",
+        "las vegas": "America/Los_Angeles",
+        "toronto": "America/Toronto",
+        "mexico city": "America/Mexico_City",
+        "cancun": "America/Cancun",
+        "rio de janeiro": "America/Sao_Paulo",
+        "sao paulo": "America/Sao_Paulo",
+        "santiago": "America/Santiago",
+        "buenos aires": "America/Argentina/Buenos_Aires",
+        "cusco": "America/Lima",
+        "cartagena": "America/Bogota",
+        "londres": "Europe/London",
+        "edimburgo": "Europe/London",
+        "paris": "Europe/Paris",
+        "nice": "Europe/Paris",
+        "lyon": "Europe/Paris",
+        "roma": "Europe/Rome",
+        "milao": "Europe/Rome",
+        "firenze": "Europe/Rome",
+        "veneza": "Europe/Rome",
+        "madrid": "Europe/Madrid",
+        "barcelona": "Europe/Madrid",
+        "sevilha": "Europe/Madrid",
+        "lisboa": "Europe/Lisbon",
+        "porto": "Europe/Lisbon",
+        "berlim": "Europe/Berlin",
+        "munique": "Europe/Berlin",
+        "frankfurt": "Europe/Berlin",
+        "amsterdam": "Europe/Amsterdam",
+        "bruxelas": "Europe/Brussels",
+        "viena": "Europe/Vienna",
+        "praga": "Europe/Prague",
+        "budapeste": "Europe/Budapest",
+        "varsovia": "Europe/Warsaw",
+        "zurique": "Europe/Zurich",
+        "genebra": "Europe/Zurich",
+        "copenhague": "Europe/Copenhagen",
+        "estocolmo": "Europe/Stockholm",
+        "oslo": "Europe/Oslo",
+        "helsinque": "Europe/Helsinki",
+        "atenas": "Europe/Athens",
+        "santorini": "Europe/Athens",
+        "dublin": "Europe/Dublin",
+        "monaco": "Europe/Monaco",
+        "rekyjavik": "Atlantic/Reykjavik",
+        "istambul": "Europe/Istanbul",
+        "moscou": "Europe/Moscow",
+        "tokyo": "Asia/Tokyo",
+        "seul": "Asia/Seoul",
+        "beijing": "Asia/Shanghai",
+        "hong kong": "Asia/Hong_Kong",
+        "bangkok": "Asia/Bangkok",
+        "singapura": "Asia/Singapore",
+        "dubai": "Asia/Dubai",
+        "sidney": "Australia/Sydney",
+        "cairo": "Africa/Cairo",
+        "marrakech": "Africa/Casablanca",
+        "cape town": "Africa/Johannesburg"
+    }
+    '''
 
     city_lower = city.lower()
 
